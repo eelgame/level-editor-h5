3458e39b5fa4f6c45f17175644a2b6714a750e4b
jude.wang <eelgame@qq.com>
2019-11-25 09:55:52 +0800

    none

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <avue-crud ref="crud" :option="option" :data="data" @row-update="addUpdate">
+      <template slot="menuLeft">
+        <el-button size="small" @click="addRow">
+          添加10条
+        </el-button>
+      </template>
+    </avue-crud>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+  }, data() {
+    return {
+      data: [{
+        id: 0,
+        name: '张三',
+        sex: 1,
+        $cellEdit: true
+      }, {
+        id: 1,
+        name: '李四',
+        sex: 0
+      }],
+      option: {
+        keyId: 'id',
+        addBtn: false,
+        editBtn: false,
+        addRowBtn: true,
+        cellBtn: true,
+        column: [{
+          label: '姓名',
+          prop: 'name',
+          cell: true,
+          rules: [
+            {
+              required: true,
+              message: '请输入姓名',
+              trigger: 'blur'
+            }
+          ]
+        }, {
+          label: '性别',
+          prop: 'sex',
+          type: 'select',
+          dicData: [{
+            label: '男',
+            value: 0
+          }, {
+            label: '女',
+            value: 1
+          }],
+          cell: true
+        }, {
+          label: '开关',
+          prop: 'switch',
+          type: 'switch',
+          cell: true
+        }]
+      }
+    }
+  },
+  methods: {
+    addUpdate(form, index, done, loading) {
+      this.$message.success('模拟网络请求')
+      setTimeout(() => {
+        this.$message.success('关闭按钮等待')
+        loading()
+      }, 1000)
+      setTimeout(() => {
+        this.$message.success(
+          '编辑数据' + JSON.stringify(form) + '数据序号' + index
+        )
+        done()
+      }, 2000)
+    },
+    addRow() {
+      this.$message.success('正在添加，请稍后')
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.$refs.crud.rowCellAdd({
+            name: ''
+          })
+        }
+      }, 500)
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Avue from '@smallwei/avue'
+import '@smallwei/avue/lib/index.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+Vue.use(Avue)
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
